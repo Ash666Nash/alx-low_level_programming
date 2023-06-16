176048e74ff68d5303d21cdc5e10600064f94bd5
Ash666Nash <laaziz.achraf@yahoo.com>
2023-06-16 22:04:39 +0100

    here is the end

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: 0 (No Error)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i, j;
 for (i = 0; i < 9; i++)
 {
-for (j = i + 1; j < 10; j++)
+for (j = i; j < 10; j++)
 {
-putchar('0' + i);
-putchar('0' + j);
+if (i != j)
+{
+putchar(i + '0');
+putchar(j + '0');
 if (i != 8 || j != 9)
 {
-putchar(44);
+putchar(',');
 putchar(' ');
+}
 }
 }
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: 0 (No Error)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i, j, k;
-for (i = 0; i < 10; i++)
+for (i = 0; i < 8; i++)
 {
-for (j = 0 ; j < 10; j++)
+for (j = i + 1; j < 9; j++)
 {
-for(k = 1 ; k < 10; k++){
-    putchar('0' + i);
-    putchar('0' + j);
-    putchar('0' + k);
-    putchar(44);
-    putchar(' ');
-if(i == 7 && j ==8 && k==9)
+for (k = j + 1; k < 10; k++)
 {
-return 0;
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(',');
+putchar(' ');
 }
 }
 }
