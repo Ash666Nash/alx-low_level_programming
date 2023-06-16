68072feb36030caecbb4b5d535de1f69c5937b24
Ash666Nash <laaziz.achraf@yahoo.com>
2023-06-16 21:50:22 +0100

    learning new things1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,13 +3,19 @@
  * main - Entry point
  * Return: 0 (No Error)
  */
+
 int main(void)
 {
 int i;
 for (i = 0; i < 10; i++)
 {
-printf("%d", i);
+putchar('0' + i);
+if (i < 9)
+{
+putchar(44);
+putchar(' ');
 }
-printf("\n");
+}
+putchar('\n');
 return (0);
 }
